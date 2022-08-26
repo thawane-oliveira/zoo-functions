71b0b06ec496319e0bbe3de4b88b57562b88d33a
Thawane <thawaneapoliveira@gmail.com>
2022-08-26 19:19:04 -0300

    Adiciona requisito 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,25 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Se é retornada a string The zoo is open ao passar Tuesday e 09:00-AM como argumentos', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+  it('Se é retornada a string The zoo is closed ao passar Wednesday e 09:00-PM como argumentos', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+  it('Se é retornada a string The zoo is closed ao passar Monday e 09:00-AM como argumentos', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+  it('Se é retornada um objeto com todos os horários e dias, em caso de não receber argumento', () => {
+    const schedule = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(getOpeningHours()).toEqual(schedule);
+  });
+});
