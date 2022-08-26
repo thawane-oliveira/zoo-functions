afaf234c2fefaa533308ad6dcb7845dd90592d8d
Thawane <thawaneapoliveira@gmail.com>
2022-08-26 18:17:11 -0300

    Adiciona requisito bônus 15

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -16,10 +16,16 @@ describe('Testes da função HandlerElephants', () => {
   it('Se retorna o número 5 caso o parâmetro designado seja popularity', () => {
     expect(handlerElephants('popularity')).toBe(5);
   });
-  it('Se o array de dias disponíveis não contém wednesday', () => {
+  it('Se o array de dias disponíveis não contém quarta-feira', () => {
     expect(handlerElephants('availability')).not.toContain('Wednesday');
   });
   it('Se ao designar location como argumento, recebe NW', () => {
     expect(handlerElephants('location')).toEqual('NW');
+  });
+  it('Se ao designar {} como argumento, retorna Parâmetro inválido, é necessário uma string', () => {
+    expect(handlerElephants({})).toEqual('Parâmetro inválido, é necessário uma string');
+  });
+  it('Se ao designar como argumento uma string que não contemple nenhuma funcionalidade do código, retorna null', () => {
+    expect(handlerElephants('TraitorsRequiem')).toBe(null);
   });
 });
