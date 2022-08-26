769d9976d6df1a80fb0bac13ea9fbb411d8d5074
Thawane <thawaneapoliveira@gmail.com>
2022-08-26 14:20:05 -0300

    Iniciando projeto com requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  return ids.map((identifier) => data.species.find((specie) => identifier === specie.id));
 }
 
 module.exports = getSpeciesByIds;
