d2bece28f194c4360f808e05f249a8d2922e7fee
Thawane <thawaneapoliveira@gmail.com>
2022-08-26 17:16:04 -0300

    Adiciona requisito 5 completo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,15 +2,24 @@ const data = require('../data/zoo_data');
 
 const animals = data.species;
 
-function countAnimals(...animal) {
-  if (!animal.length) {
+function countAnimals(animal) {
+  if (!animal) {
     return animals.reduce((acc, specie) => {
       acc[specie.name] = specie.residents.length;
       return acc;
     }, {});
   }
+  if (animal.sex) {
+    return animals
+      .find((type) => (type.name === animal.specie)).residents
+      .filter((gender) => gender.sex === animal.sex).length;
+  }
+  return animals.find((bichineo) => bichineo.name === animal.specie).residents.length;
 }
 
-console.log(countAnimals());
-
 module.exports = countAnimals;
+
+// const acc = {};
+// animals.forEach((specie) => {
+//   acc[specie.name] = specie.residents.length;
+// });
