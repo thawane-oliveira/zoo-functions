54c7227b713cd8bab4d2d451303d5caa865db5c7
Thawane <thawaneapoliveira@gmail.com>
2022-08-26 16:37:15 -0300

    Adiciona primeira parte do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const animals = data.species;
+
 function countAnimals(...animal) {
-  // seu código aqui
+  if (!animal.length) {
+    return animals.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
