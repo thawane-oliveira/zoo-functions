dd4445ab4bcf972c60bd5569624fa56e1830af05
Thawane <thawaneapoliveira@gmail.com>
2022-08-26 15:51:09 -0300

    Adiciona final do requisito 4, funcionando completo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -8,6 +8,9 @@ function getRelatedEmployees(managerId) {
   if (!isManager(managerId)) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
+  return data.employees
+    .filter((employee) => employee.managers.includes(managerId))
+    .map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
