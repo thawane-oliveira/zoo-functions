561649f9d2170edc1119cc04350831510d2d9b4a
Thawane <thawaneapoliveira@gmail.com>
2022-08-26 14:50:47 -0300

    Adiciona requisito 2

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  // return data.employees.forEach((manager) => .some(() => ));
 }
 
 function getRelatedEmployees(managerId) {
