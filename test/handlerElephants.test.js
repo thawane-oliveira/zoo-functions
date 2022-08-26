599b0d7f7456cbfe0f00d31232648e214ac9f903
Thawane <thawaneapoliveira@gmail.com>
2022-08-26 18:07:51 -0300

    Adiciona requisito 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -13,7 +13,13 @@ describe('Testes da função HandlerElephants', () => {
   it('Se retorna undefined caso não tenha nenhum parâmetro designado', () => {
     expect(handlerElephants()).toBe(undefined);
   });
-  it('Se retorna undefined caso não tenha nenhum parâmetro designado', () => {
-    expect(handlerElephants()).toBe(undefined);
+  it('Se retorna o número 5 caso o parâmetro designado seja popularity', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('Se o array de dias disponíveis não contém wednesday', () => {
+    expect(handlerElephants('availability')).not.toContain('Wednesday');
+  });
+  it('Se ao designar location como argumento, recebe NW', () => {
+    expect(handlerElephants('location')).toEqual('NW');
   });
 });
