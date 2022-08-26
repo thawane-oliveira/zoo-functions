315ab25b3452ccf3824654ff2ccf417956a436ad
Thawane <thawaneapoliveira@gmail.com>
2022-08-26 18:00:11 -0300

    Adiciona parte do requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,19 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Se a média retornada por averageAge é próxima de 10.5', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  it('Se o número retornado por count é 4', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  it('Se o argumento names retorna um array de nome de residentes, que contenha Jefferson', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('Se retorna undefined caso não tenha nenhum parâmetro designado', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+  it('Se retorna undefined caso não tenha nenhum parâmetro designado', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+});
