96f73b8a7d478b20a85ce59399fa9822497249fe
Thawane <thawaneapoliveira@gmail.com>
2022-08-28 14:07:51 -0300

    Adiciona requisito 10 funcionando

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
+// const { employees } = data;
+
+const getOldestAnimal = (specie) =>
+  (specie.residents.sort((yare, ora) => ora.age - yare.age))[0];
+
+// function getOldestAnimal (specie) {
+// specie.residents.sort((yare, ora) => ora.age - yare.ora[0]};
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const idOwner = employees.find((employee) => employee.id === id);
+  const care = species.find((specie) => specie.id === idOwner.responsibleFor[0]);
+  return [getOldestAnimal(care).name, getOldestAnimal(care).sex, getOldestAnimal(care).age];
 }
 
 module.exports = getOldestFromFirstSpecies;
+
+// idOwner é o funcionário, responsibleFor retorna os animais que ele cuida e o 0 é o index do primeiro animal
+// Array esperado pelo teste: nome, sexo e por último idade
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
+const { species } = data;
 
+const { hours } = data;
+
+// function get(){};
+
+function getSchedule(scheduleTarget) {
+  if (!scheduleTarget || scheduleTarget !== { species } || scheduleTarget !== { hours }) {
+    return {};
+  }
 }
 
 module.exports = getSchedule;
