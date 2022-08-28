205307c9bd5f545b70c7a02b562cac929b584c82
Thawane <thawaneapoliveira@gmail.com>
2022-08-28 15:55:44 -0300

    Adiciona requisito 11 refatorado e funcionando

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,22 +2,27 @@ const data = require('../data/zoo_data');
 
 const { species, employees } = data;
 
-const arrivedercci = employees.map((emp) => {
-  const result = {
+const completeInfo = employees.map((emp) => {
+  const infoChart = {
     id: emp.id,
     fullName: `${emp.firstName} ${emp.lastName}`,
     species: emp.responsibleFor.map((x) => species.find((animal) => animal.id === x).name),
     locations: emp.responsibleFor.map((x) => species.find((animal) => animal.id === x).location),
   };
-  return result;
+  return infoChart;
 });
 
-// const seguindo o modelo solicitado no readme
+// const completeInfo seguindo o modelo solicitado no readme, de id, nome completo, espécies e suas localizações
 
-function getEmployeesCoverage(param) {
-  if (!param || param === undefined) {
-    return arrivedercci;
-  }
+function getEmployeesCoverage(employee) {
+  const info = completeInfo;
+  if (!employee) return info;
+  const result = info.find((e) =>
+    e.fullName.includes(Object.values(employee)) || e.id.includes(Object.values(employee)));
+  if (!result) throw new Error('Informações inválidas');
+  return result;
 }
 
 module.exports = getEmployeesCoverage;
+
+// consultado sobre includes em https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
