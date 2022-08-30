573c3b2b1285f33b1b7a9c666f3adb432c1d1527
Thawane <thawaneapoliveira@gmail.com>
2022-08-30 12:17:16 -0300

    Adiciona mais dois subrequisitos do requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,16 +1,30 @@
 const data = require('../data/zoo_data');
 
-// const { species } = data;
+const { species } = data;
 
-const location = {
+const noParam = {
   NE: ['lions', 'giraffes'],
   NW: ['tigers', 'bears', 'elephants'],
   SE: ['penguins', 'otters'],
   SW: ['frogs', 'snakes'],
 };
 
+const animals = () => {
+  const animalGrid = {
+    NE: species.filter((specie) => specie.location === 'NE').map((animal) => animal.name),
+    NW: species.filter((specie) => specie.location === 'NW').map((animal) => animal.name),
+    SE: species.filter((specie) => specie.location === 'SE').map((animal) => animal.name),
+    SW: species.filter((specie) => specie.location === 'SW').map((animal) => animal.name),
+  };
+  return animalGrid;
+};
+
+const animalSex = () => {
+
+};
+
 function getAnimalMap(options) {
-  if (!options) return location;
+  if (!options || options !== { includeNames: true }) return noParam;
 }
 
 module.exports = getAnimalMap;
